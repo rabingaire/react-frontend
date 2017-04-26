bcb287d14a75351481f2f064de2e00c5bd72f484
Rabin Gaire <rabingaire20@gmail.com>
2017-04-26 19:12:02 +0545

    added babel in the project

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,21 @@ modules.exports = {
     output: {
         path : __dirname,
         filename: 'bundle.js'
+    }, 
+    modules: {
+        loaders: [
+            {test: /\.css$/, use: 'style!css'},
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'es2015', 'stage-0', 'react'],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            }
+        ]
     }
 };
