5c2276ae6e19aee3a51c32d9022ab505c09c6c8b
Rabin Gaire <rabingaire20@gmail.com>
2017-04-27 20:29:06 +0545

    basic code structure defined (webpack setup)

--- entry.js
+++ entry.js
@@ -1,2 +1,15 @@
-document.write("Hello World");
-document.write("Rabin");
+import React from 'react';
+import { render } from 'react-dom';
+
+class Entry extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World
+            </div>
+        );
+    }
+};
+
+var root = document.getElementById('root') 
+render(<Entry />, root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
-var path = require('path');
-
 module.exports = {
     entry: './entry.js',
     output: {
-        path : path.resolve(__dirname, 'dist'),
+        path : __dirname,
         filename: 'bundle.js'
     }, 
     module: {
         rules: [
-            {test: /\.css$/, use: 'style!css'},
+            {
+              test: /\.css$/,
+              use: [
+                { loader: 'style-loader' },
+                { loader: 'css-loader' }
+              ],
+            },
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env', 'es2015', 'stage-0'],
+                        presets: ['env', 'es2015', 'stage-0', 'react'],
                         plugins: ['transform-runtime']
                     }
                 }
